f75680c16155067e1c1e7067ea53ea1168e8159c
wiktor-falek <falekwiktor@gmail.com>
2022-06-02 22:42:45 +0200

    add socket.io

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import * as url from 'url';
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 import express, { json, urlencoded } from "express";
+import http from "http";
+import { Server } from 'socket.io';
 import dotenv from "dotenv"; dotenv.config();
 import cors from "cors";
 import path from "path";
@@ -8,7 +10,7 @@ import path from "path";
 import logger from "./logger.js";
 
 
-// initialize express
+// express
 const app = express();
 
 app.use(cors());
@@ -19,6 +21,17 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, 'static'), { extensions: ['html']}));
 
-const server = app.listen(process.env.PORT || 3000, () => {
+// socket.io
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+    logger.info("user connected");
+    socket.on("disconnect", () => {
+        logger.info("user disconnected");
+    });
+});
+
+server.listen(process.env.PORT || 3000, () => {
     logger.info(`listening on port ${server.address().port}`);
 });
