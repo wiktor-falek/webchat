977773dc40d2a925d2645dfbcdd666f279dddc1b
wiktor-falek <falekwiktor@gmail.com>
2022-06-16 17:37:53 +0200

    additional logging, code cleanup

--- src/ClientStorage.js
+++ src/ClientStorage.js
@@ -1,6 +1,6 @@
 import { v4 as uuidv4 } from 'uuid';
-import uuidIsValid from './utils/uuidIsValid.js';
 
+import uuidIsValid from './utils/uuidIsValid.js';
 import logger from "./logger.js";
 import Client from "./Client.js";
 
@@ -9,19 +9,23 @@ class ClientStorage {
 
     addClient(name, color, clientId=undefined) {
         // instantiates and returns a new client with unique id
-        let id = uuidv4();
+        let id;
         if (uuidIsValid(clientId)) {
             logger.debug(`Client(${name, clientId}) provided his id`);
             id = clientId; // if validated set id to uuid provided by client
         }
+        else {
+            id = uuidv4();
+        }
         const client = new Client(id, name, color);
         this.#clients[id] = client;
         return client;
+        logger.debug(`Client(${client.name}, ${client.id}) added to ClientStorage`);
     }
 
     removeClient(client) {
         delete this.#clients[client.id];
-        logger.debug(`Client(${client.name}, ${client.id}) removed`);
+        logger.debug(`Client(${client.name}, ${client.id}) removed from ClientStorage`);
     }
 
     getClient(id) {
