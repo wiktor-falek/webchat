99f1d6725759020e7909e135eef244f7853bc19e
wiktor-falek <falekwiktor@gmail.com>
2022-07-22 04:26:49 +0200

    validate message by max length and trim

--- src/utils/usernameIsValid.js
+++ src/utils/usernameIsValid.js
@@ -1,14 +1,18 @@
 import dotenv from 'dotenv'; dotenv.config();
 
 function validateName(name) {
+    name = name.trim();
     if (
         name !== undefined &&
         name !== null &&
         name !== "null" && 
         name !== "undefined" &&
+        name !== "" &&
+        name !== " " &&
+        name.length <= process.env.MESSAGE_MAXLEN || 32 &&
         name.toUpperCase() !== "[SERVER]"
         ) {
-            return name.trim();
+            return name;
         }
     return undefined;
 }
